b8886270dc53cae4b4453fe944aa349ee00dfc1e
snat-s <santpedrozadiaz@gmail.com>
2023-10-08 20:09:44 -0600

    changed the port option

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const wordSimilarity = require('./word_similarity');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const server = http.createServer(async (req,res) => {
 	if (req.url === '/similarities' && req.method === 'GET') {
@@ -24,6 +23,6 @@ const server = http.createServer(async (req,res) => {
 	}
 });
 
-server.listen(port, hostname, () => {
+server.listen(port, () => {
 	console.log('server running at port 3000');
 });
